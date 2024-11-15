fea7d412be20ebb39d2cb22913dd0585b2a7056b
ryone9re <ryoichi.kurimoto@gmail.com>
2024-11-15 08:57:47 +0000

    Fix vite config

--- streamlit_carousel_slider/frontend/vite.config.ts
+++ streamlit_carousel_slider/frontend/vite.config.ts
@@ -1,14 +1,15 @@
-import react from '@vitejs/plugin-react-swc';
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react-swc";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   server: { host: "127.0.0.1" },
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer]
-    }
-  }
-})
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+});
