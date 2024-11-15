6fed29c1def69a9084e18fbf70c9b209163ac524
ryone9re <ryoichi.kurimoto@gmail.com>
2024-11-15 02:19:44 +0000

    Add document and example

---
+++ example.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from streamlit_carousel_slider import streamlit_carousel_slider
+
+test_images = [{"sourse": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhPbO0Hwiy3nXmTGo-Eo66iqRnhgjLXB2sD7huKl5pjltjVNddTEmPdETiYbFRAvUL80XPE8_2SDJeW-EP8kgLfVBjTMb_GIDVWn3V4Cq3O5fLacfWRam-72uD9oRGzlPJccNQN0e89PH7aMm5lcDuejTHw4HZWV7uB9aU-K0X6e9wxsGwRmE0K_TI0rpwQ/s1000/shinnen_aisatsu_hebi_medousa_couple.png"}]
+
+st.write(streamlit_carousel_slider(images=test_images))
--- streamlit_carousel_slider/__init__.py
+++ streamlit_carousel_slider/__init__.py
@@ -1,23 +1,39 @@
 import os
+from typing import TypedDict
 import streamlit.components.v1 as components
 
 _RELEASE = False
 
-COMPONENT_NAME = "streamlit_carousel_slider"
+_COMPONENT_NAME = "streamlit_carousel_slider"
 
 if not _RELEASE:
     _component_func = components.declare_component(
-        COMPONENT_NAME,
+        _COMPONENT_NAME,
         url="http://localhost:3000",
     )
 else:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, "frontend/dist")
     _component_func = components.declare_component(
-        COMPONENT_NAME,
+        _COMPONENT_NAME,
         path=build_dir
     )
 
 
-def streamlit_carousel_slider(images):
-    _component_func(images)
+class Image(TypedDict):
+    """
+    An image to be displayed in carousel.
+    """
+    sourse: str
+    """Image sourse string"""
+
+
+def streamlit_carousel_slider(images: list[Image]) -> None:
+    """
+    Carousel slider component entrypoint for streamlit using React and tailwind with daisyui.
+
+    :param list[Image] images: A list of images to be displayed in the carousel.
+    :return: Nothing to be returned.
+    :rtype: None
+    """
+    return _component_func(images)
