9c9d7a68ba33c13a834e58fa44c65ed2a6c7d347
ryone9re <ryoichi.kurimoto@gmail.com>
2024-11-15 04:21:38 +0000

    Add indicator

--- streamlit_carousel_slider/frontend/src/carousel-slider.tsx
+++ streamlit_carousel_slider/frontend/src/carousel-slider.tsx
@@ -34,35 +34,48 @@ export function CarouselSlider({ images }: CarouselSliderProps): ReactNode {
   );
 
   return (
-    <div className="carousel w-full">
-      {images.map((image, index) => (
-        <div
-          key={image.source}
-          id={`carousel-slide-${index}`}
-          className="carousel-item relative w-full"
-        >
-          <img
-            alt={`carousel-image-${index}`}
-            src={image.source}
-            className="w-full"
-          />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a
-              href={`#carousel-slide-${previousSlide(index)}`}
-              className="btn btn-circle"
-            >
-              ❮
-            </a>
-            <a
-              href={`#carousel-slide-${nextSlide(index)}`}
-              className="btn btn-circle"
-            >
-              ❯
-            </a>
+    <>
+      <div className="carousel w-full">
+        {images.map((image, index) => (
+          <div
+            key={image.source}
+            id={`carousel-slide-${index}`}
+            className="carousel-item relative w-full"
+          >
+            <img
+              alt={`carousel-image-${index}`}
+              src={image.source}
+              className="w-full"
+            />
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a
+                href={`#carousel-slide-${previousSlide(index)}`}
+                className="btn btn-circle"
+              >
+                ❮
+              </a>
+              <a
+                href={`#carousel-slide-${nextSlide(index)}`}
+                className="btn btn-circle"
+              >
+                ❯
+              </a>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <div className="flex w-full justify-center gap-2 py-2">
+        {images.map((image, index) => (
+          <a
+            key={image.source}
+            href={`#carousel-slide-${index}`}
+            className="btn btn-xs"
+          >
+            {index}
+          </a>
+        ))}
+      </div>
+    </>
   );
 }
 
