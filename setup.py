ad24b2db7b5d74c59d7e5f87e899fde8d2262c86
ryone9re <ryoichi.kurimoto@gmail.com>
2024-11-25 03:53:32 +0000

    Version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-carousel-slider",
-    version="1.2.2",
+    version="1.3.0",
     author="ryone9re",
     author_email="ryoichi.kurimoto@gmail.com",
     description="Carousel slider component for streamlit using React and tailwind with daisyui.",
